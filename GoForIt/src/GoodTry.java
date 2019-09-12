a5e7e7ec70b1059e9d5e221ffdc49b64008e4121
Michael Liguori <s1276872@monmouth.edu>
2019-09-11 20:12:29 -0400

    Good to Go

--- GoForIt/src/GoodTry.java
+++ GoForIt/src/GoodTry.java
@@ -2,11 +2,12 @@
 public class GoodTry {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		System.out.println("Hello World");
 		
 		System.out.println("Bye World!");
+		
+		System.out.println("Ta-ta");
 
 	}
 
