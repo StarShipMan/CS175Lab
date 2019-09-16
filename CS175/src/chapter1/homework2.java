9c2a941dc65dcc372045df68cddf5b40e5708899
MikiM <MikiM@10.71.5.177>
2019-09-16 19:51:25 -0400

    Homework for Chapter 1

---
+++ CS175/src/chapter1/homework2.java
@@ -0,0 +1,12 @@
+package chapter1;
+
+public class homework2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print(1+2+3+4+5+6+7+8+9+10);
+		// prints sum of first ten positive integers to the console.
+	}
+
+}
---
+++ CS175/src/chapter1/homework4.java
@@ -0,0 +1,14 @@
+package chapter1;
+
+public class homework4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("===========");
+		System.out.println("||Michael||");
+		System.out.println("===========");
+		// prints name surrounded by a box to the console.
+	}
+
+}
