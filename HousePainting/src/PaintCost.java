40fe3452df07061ef7afd56543c223dfc4e40b2d
Michael Liguori <s1276872@monmouth.edu>
2019-09-16 20:57:41 -0400

    Variable Set

---
+++ HousePainting/src/PaintCost.java
@@ -0,0 +1,27 @@
+
+public class PaintCost {
+
+	public static void main(String[] args) {
+		/*
+		 * This program will calculate the cost of painting a house
+		 */
+		double houseLengthFeet;
+		double houseLengthInch;
+		double houseWidthFeet;
+		double houseWidthInch;
+		double houseHeightFeet;
+		double houseHeightInch;
+		double windowLengthFeet;
+		double windowLengthInch;
+		double windowWidthFeet;
+		double windowWidthInch;
+		double doorLengthFeet;
+		double doorLengthInch;
+		double doorWidthFeet;
+		double doorWidthInch;
+		int numberOfWindows;
+		int numberoFDoors;
+		double cost;
+	}
+
+}
