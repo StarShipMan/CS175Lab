b56d41a14327f3968c3ec1aadcee6ae95aeca24c
MikiM <MikiM@10.71.35.225>
2020-01-27 20:19:59 -0500

    Commiting old changes

---
+++ CS175/src/chapter1/HelloPrinter.java
@@ -0,0 +1,18 @@
+package chapter1;
+
+public class HelloPrinter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello", "World");
+		Syste.out.println("Hello World");
+		System.out.println(Hello World);
+		/*
+		 * There are three compile-time errors in the above examples, 
+		 * The first has too many arguments for the println method.
+		 * The second has a spelling error for the method being called.
+		 * The third is missing quotations around the string Hello World.
+		 */
+	}
+
+}
